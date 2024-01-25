6fe7a07c8549bfa48d2388ec27e36c570cd252c7
renyongdong <ryd01413914@alibaba-inc.com>
2024-01-25 16:25:19 +0800

    import-data right

--- packages/studio-website/src/pages/instance/import-data/index.tsx
+++ packages/studio-website/src/pages/instance/import-data/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import { Row, Col,Segmented, Collapse, Flex ,Button,Divider} from 'antd';
+import { Row, Col, Segmented, Collapse, Flex, Button, Divider, Card, Space, Tooltip } from 'antd';
 import { createFromIconfontCN } from '@ant-design/icons';
 import { SegmentedValue } from 'antd/es/segmented';
+import GraphInsight from '../create-instance/create-schema/graph-view';
 const IconFont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/a/font_4377140_slis0xqmzfo.js',
 });
@@ -30,21 +31,36 @@ const ImportData: React.FunctionComponent<IImportDataProps> = props => {
     },
   ];
   return (
-    <Row style={{ padding: '24px' }} gutter={[24,24]}>
+    <Row style={{ padding: '24px' }} gutter={[32, 16]}>
       <Col span={16}>
-      <Flex gap="middle" justify='space-between'>
-        <Segmented options={option} onChange={nodeEdgeChange} />
-        <Button >开始导入</Button>
-      </Flex>
-      <Divider style={{margin:'12px 0px'}}/>
-      <Collapse>
-        <Panel key={'1'} header={<>label:{'user'}</>} extra={genExtra()}>
-          test
-        </Panel>
-      </Collapse>
+        <Flex gap="middle" justify="space-between">
+          <Segmented options={option} onChange={nodeEdgeChange} />
+          <Tooltip placement="topRight" title="您需要先绑定全部的点边数据源才可以导入数据">
+            <Button>开始导入</Button>
+          </Tooltip>
+        </Flex>
+        <Divider style={{ margin: '12px 0px' }} />
+        <Collapse>
+          <Panel key={'1'} header={<>label:{'user'}</>} extra={genExtra()}>
+            test
+          </Panel>
+        </Collapse>
       </Col>
       <Col span={8}>
-
+        <Flex gap="middle" justify="space-between">
+          <>预览</>
+          <Space>
+            <Tooltip placement="topRight" title="导入「数据导入」的配置文件">
+              <Button>导入配置</Button>
+            </Tooltip>
+            <Tooltip placement="topRight" title="导出「数据导入」的配置文件">
+              <Button>导出配置</Button>
+            </Tooltip>
+          </Space>
+        </Flex>
+        <Card style={{marginTop:'24px'}}>
+          <GraphInsight children={<p style={{textAlign:'center',margin:'0px'}}>目前绑定了 1 条边，2个点</p>}/>
+        </Card>
       </Col>
     </Row>
   );
