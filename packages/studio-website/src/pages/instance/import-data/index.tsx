ecd0cdb938d4c18e82ba74f7e94a3e28940672f5
renyongdong <ryd01413914@alibaba-inc.com>
2024-01-25 16:09:47 +0800

    import-data left

--- packages/studio-website/src/pages/instance/import-data/index.tsx
+++ packages/studio-website/src/pages/instance/import-data/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import { Space, Segmented, Collapse } from 'antd';
+import { Row, Col,Segmented, Collapse, Flex ,Button,Divider} from 'antd';
 import { createFromIconfontCN } from '@ant-design/icons';
-import AddDisposition from './add-disposition';
 import { SegmentedValue } from 'antd/es/segmented';
 const IconFont = createFromIconfontCN({
   scriptUrl: '//at.alicdn.com/t/a/font_4377140_slis0xqmzfo.js',
@@ -9,30 +8,45 @@ const IconFont = createFromIconfontCN({
 interface IImportDataProps {}
 const { Panel } = Collapse;
 const ImportData: React.FunctionComponent<IImportDataProps> = props => {
-  const nodeEdgeChange:(value: SegmentedValue) => void= (val) => {};
+  const nodeEdgeChange: (value: SegmentedValue) => void = val => {};
   const genExtra: () => React.ReactNode = () => {
-    return <div><IconFont type="icon-baocun" /><IconFont type="icon-bangding" /><IconFont type="icon-ziyuan" /><IconFont type="icon-jiechubangding" /></div>;
+    return (
+      <div>
+        {/* <IconFont type="icon-baocun" /> */}
+        <IconFont type="icon-bangding" />
+        {/* <IconFont type="icon-ziyuan" /> */}
+        <IconFont type="icon-jiechubangding" />
+      </div>
+    );
   };
+  const option: { label: string; value: string }[] = [
+    {
+      label: '点数据源绑定（0/2）',
+      value: 'nodesource',
+    },
+    {
+      label: '边数据源绑定（0/2）',
+      value: 'edgesource',
+    },
+  ];
   return (
-    <div style={{ paddingRight: '24px' }}>
-      <div>
-        <Space>
-          <span>数据源类型</span>
-          <Segmented options={['ODPS', '本地文件','OSS']} defaultValue="ODPS" style={{ marginBottom: '16px' }} onChange={nodeEdgeChange}/> 
-        </Space>
-      </div>
-      <div>
-        <Space>
-          <span>绑定数据源</span>
-          <Segmented options={['Node Label', 'Edge labels']} defaultValue="Node Label" style={{ marginBottom: '16px' }} onChange={nodeEdgeChange}/> 
-        </Space>
-        <Collapse defaultActiveKey={['1']}>
-          <Panel key={'1'} header={undefined} extra={genExtra()}>
-            <AddDisposition />
-          </Panel>
-        </Collapse>
-      </div>
-    </div>
+    <Row style={{ padding: '24px' }} gutter={[24,24]}>
+      <Col span={16}>
+      <Flex gap="middle" justify='space-between'>
+        <Segmented options={option} onChange={nodeEdgeChange} />
+        <Button >开始导入</Button>
+      </Flex>
+      <Divider style={{margin:'12px 0px'}}/>
+      <Collapse>
+        <Panel key={'1'} header={<>label:{'user'}</>} extra={genExtra()}>
+          test
+        </Panel>
+      </Collapse>
+      </Col>
+      <Col span={8}>
+
+      </Col>
+    </Row>
   );
 };
 
