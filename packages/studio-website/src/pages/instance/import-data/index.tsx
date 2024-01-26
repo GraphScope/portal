aeb891be8bb4530a9289f7343443ab215943577f
renyongdong <ryd01413914@alibaba-inc.com>
2024-01-26 15:44:14 +0800

    create:数据导入入口页面

--- packages/studio-website/src/pages/instance/import-data/index.tsx
+++ packages/studio-website/src/pages/instance/import-data/index.tsx
@@ -1,38 +1,50 @@
 import * as React from 'react';
-import { Space, Segmented, Collapse } from 'antd';
-import { createFromIconfontCN } from '@ant-design/icons';
-import AddDisposition from './add-disposition';
+import { Row, Col, Segmented, Flex, Button, Divider, Card, Space, Tooltip, Typography } from 'antd';
 import { SegmentedValue } from 'antd/es/segmented';
-const IconFont = createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/a/font_4377140_slis0xqmzfo.js',
-});
+import GraphInsight from '../create-instance/create-schema/graph-view';
 interface IImportDataProps {}
-const { Panel } = Collapse;
+const { Text } = Typography;
 const ImportData: React.FunctionComponent<IImportDataProps> = props => {
-  const nodeEdgeChange:(value: SegmentedValue) => void= (val) => {};
-  const genExtra: () => React.ReactNode = () => {
-    return <div><IconFont type="icon-baocun" /><IconFont type="icon-bangding" /><IconFont type="icon-ziyuan" /><IconFont type="icon-jiechubangding" /></div>;
-  };
+  const nodeEdgeChange: (value: SegmentedValue) => void = val => {};
+  const option: { label: string; value: string }[] = [
+    {
+      label: '点数据源绑定（0/2）',
+      value: 'nodesource',
+    },
+    {
+      label: '边数据源绑定（0/1）',
+      value: 'edgesource',
+    },
+  ];
   return (
-    <div style={{ paddingRight: '24px' }}>
-      <div>
-        <Space>
-          <span>数据源类型</span>
-          <Segmented options={['ODPS', '本地文件','OSS']} defaultValue="ODPS" style={{ marginBottom: '16px' }} onChange={nodeEdgeChange}/> 
-        </Space>
-      </div>
-      <div>
-        <Space>
-          <span>绑定数据源</span>
-          <Segmented options={['Node Label', 'Edge labels']} defaultValue="Node Label" style={{ marginBottom: '16px' }} onChange={nodeEdgeChange}/> 
-        </Space>
-        <Collapse defaultActiveKey={['1']}>
-          <Panel key={'1'} header={undefined} extra={genExtra()}>
-            <AddDisposition />
-          </Panel>
-        </Collapse>
-      </div>
-    </div>
+    <Row style={{ padding: '24px' }} gutter={32}>
+      <Col span={16}>
+        <Flex gap="middle" justify="space-between">
+          <Segmented options={option} onChange={nodeEdgeChange} />
+          <Tooltip placement="topRight" title="您需要先绑定全部的点边数据源才可以导入数据">
+            <Button>开始导入</Button>
+          </Tooltip>
+        </Flex>
+        <Divider style={{ margin: '12px 0px' }} />
+        <>数据源</>
+      </Col>
+      <Col span={8}>
+        <Flex gap="middle" justify="space-between" align="center">
+          <Text type="secondary">预览</Text>
+          <Space>
+            <Tooltip placement="topRight" title="导入「数据导入」的配置文件">
+              <Button>导入配置</Button>
+            </Tooltip>
+            <Tooltip placement="topRight" title="导出「数据导入」的配置文件">
+              <Button>导出配置</Button>
+            </Tooltip>
+          </Space>
+        </Flex>
+        <Card style={{ marginTop: '24px', border: '1px dashed #000', borderRadius: '0px' }}>
+          GraphInsight
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
