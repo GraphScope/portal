d6353c24905c23fc1f68d05cd096d7d1c16349a5
renyongdong <ryd01413914@alibaba-inc.com>
2024-01-26 12:01:36 +0800

    create StagesImportPackages

---
+++ packages/studio-website/src/pages/instance/import-data/stages-import-packages.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Segmented, Card } from 'antd';
+
+type StagesImportPackagesProps = {};
+const StagesImportPackages: React.FunctionComponent<StagesImportPackagesProps> = props => {
+  return (
+    <>
+      <Segmented options={[]} />
+      <Card>
+       
+      </Card>
+    </>
+  );
+};
+export default StagesImportPackages;
