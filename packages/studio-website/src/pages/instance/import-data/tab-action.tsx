72213f14a595583a4b5b83e47ba33bdea5fa4cbe
renyongdong <ryd01413914@alibaba-inc.com>
2024-02-05 18:34:28 +0800

    feat: 数据导入tab组件封装

--- packages/studio-website/src/pages/instance/import-data/tab-action.tsx
+++ packages/studio-website/src/pages/instance/import-data/tab-action.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Space, Typography, theme } from 'antd';
-import { useContext } from './useContext';
-type ITabActionProps = {};
+type ITabActionProps = {
+  items: {
+    label: string;
+    value: string;
+  }[];
+  tabChange(val: string): void;
+};
 const { Text } = Typography;
 const { useToken } = theme;
 
 const TabAction: React.FC<ITabActionProps> = props => {
+  const { items, tabChange } = props;
   const { token } = useToken();
-  const { store, updateStore } = useContext();
-  const {
-    currentType,
-    sourceList: { nodes, edges },
-  } = store;
+  const [currentType, updatCurrentType] = useState(items[0].value);
   const styles: React.CSSProperties = {
     borderBottom: `2px solid ${token.colorPrimary}`,
     paddingBottom: '7px',
@@ -20,24 +22,20 @@ const TabAction: React.FC<ITabActionProps> = props => {
     borderBottom: 'none',
     paddingBottom: '7px',
   };
-
   const handleChange = (val: string) => {
-    updateStore(draft => {
-      draft.currentType = val === 'Vertices' ? 'node' : 'edge';
-    });
+    updatCurrentType(val);
+    tabChange(val);
   };
-  const verticesStyle = currentType === 'node';
-
-  const bindNodeCount = nodes.filter(item => item.isBind).length;
-  const bindEdgeCount = edges.filter(item => item.isBind).length;
   return (
     <Space>
-      <Text style={verticesStyle ? styles : activeStyles} onClick={() => handleChange('Vertices')}>
-        Point Data Eource Binding（{bindNodeCount}/{nodes?.length}）
-      </Text>
-      <Text style={!verticesStyle ? styles : activeStyles} onClick={() => handleChange('Edges')}>
-        Edge Data Eource Binding({bindEdgeCount}/{edges?.length})
-      </Text>
+      {items.map(item => {
+        const { label, value } = item;
+        return (
+          <Text style={currentType === value ? styles : activeStyles} onClick={() => handleChange(value)}>
+            {label}
+          </Text>
+        );
+      })}
     </Space>
   );
 };
