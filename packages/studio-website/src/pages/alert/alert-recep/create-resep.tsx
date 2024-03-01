06c7a1df835cd671441936ef7d62e9699a77e06f
renyongdong <ryd01413914@alibaba-inc.com>
2024-03-01 17:01:48 +0800

    feat: 告警规则国际化，状态字段调整

--- packages/studio-website/src/pages/alert/alert-recep/create-resep.tsx
+++ packages/studio-website/src/pages/alert/alert-recep/create-resep.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Button, Checkbox, Form, Input, Select, Modal, Flex, message } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Checkbox, Form, Input, Select, Modal, Flex, message, Tooltip, Switch } from 'antd';
 import { FormattedMessage } from 'react-intl';
 import { registerReceiver, updateReceiverById } from '../service';
 import { Item } from './index';
@@ -20,8 +20,23 @@ type ICreateRecepProps = {
 const CreateRecep: React.FC<ICreateRecepProps> = props => {
   const { isCreateRecep, handelChange, editDatas } = props;
   const [form] = Form.useForm();
+  const [state, updateState] = useState({
+    status: false,
+    is_at_all: false,
+  });
+  const { status, is_at_all } = state;
   useEffect(() => {
-    Object.keys(editDatas).length > 0 && form.setFieldsValue(editDatas);
+    if (Object.keys(editDatas).length > 0) {
+      const { enable, is_at_all } = editDatas;
+      form.setFieldsValue(editDatas);
+      updateState(preset => {
+        return {
+          ...preset,
+          is_at_all: is_at_all,
+          status: enable,
+        };
+      });
+    }
   }, []);
   const onFinish = async () => {
     /** 编辑告警接收 */
@@ -29,13 +44,18 @@ const CreateRecep: React.FC<ICreateRecepProps> = props => {
       const { receiver_id } = editDatas;
       const { at_user_ids } = form.getFieldsValue();
       const userid = Array.isArray(at_user_ids) ? at_user_ids : at_user_ids.split(',');
-      const data = { ...form.getFieldsValue(), at_user_ids: userid };
+      const data = { ...form.getFieldsValue(), at_user_ids: userid, enable: status, is_at_all };
       const res = await updateReceiverById(receiver_id, data);
       await message.success(res);
     } else {
       /** 创建告警接收 */
       const { at_user_ids } = form.getFieldsValue();
-      const res = await registerReceiver({ ...form.getFieldsValue(), at_user_ids: [at_user_ids], enable: true });
+      const res = await registerReceiver({
+        ...form.getFieldsValue(),
+        at_user_ids: [at_user_ids],
+        enable: true,
+        is_at_all,
+      });
       await message.success(res);
     }
     handelChange(false);
@@ -71,10 +91,34 @@ const CreateRecep: React.FC<ICreateRecepProps> = props => {
           <Input />
         </Form.Item>
         <Form.Item<FieldType> label={<FormattedMessage id="Is At All" />} name="is_at_all" valuePropName="checked">
-          <Checkbox />
+          <Tooltip title={is_at_all ? 'enable' : 'disable'}>
+            <Switch
+              value={is_at_all}
+              onChange={e => {
+                updateState(preset => {
+                  return {
+                    ...preset,
+                    is_at_all: e,
+                  };
+                });
+              }}
+            />
+          </Tooltip>
         </Form.Item>
         <Form.Item<FieldType> label={<FormattedMessage id="Status" />} name="enable" valuePropName="checked">
-          <Checkbox />
+          <Tooltip title={status ? 'enable' : 'disable'}>
+            <Switch
+              value={status}
+              onChange={e => {
+                updateState(preset => {
+                  return {
+                    ...preset,
+                    status: e,
+                  };
+                });
+              }}
+            />
+          </Tooltip>
         </Form.Item>
       </Form>
       <Flex justify="center">
