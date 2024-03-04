d4fd22d58e0676467e0b5a178834ae2b9eb9e7bb
renyongdong <ryd01413914@alibaba-inc.com>
2024-03-04 12:03:29 +0800

    feat: 修改引擎样式，是否可以编辑表格isEditable

--- packages/studio-importor/src/properties-editor/edit-table/index.tsx
+++ packages/studio-importor/src/properties-editor/edit-table/index.tsx
@@ -2,7 +2,7 @@ import { Form, Input, Select, Table } from 'antd';
 import type { FormInstance } from 'antd/es/form';
 import React, { createContext, useContext, useEffect, memo, useRef, forwardRef } from 'react';
 import { EditOutlined } from '@ant-design/icons';
-import { EditableCellProps, EditableRowProps ,ConfigColumns,PropertyList} from '../interface';
+import { EditableCellProps, EditableRowProps, ConfigColumns, PropertyList } from '../interface';
 import { EditType } from '../mapdata';
 
 const EditableContext = createContext<FormInstance<any> | null>(null);
@@ -19,7 +19,7 @@ const TableContext = createContext<{
   },
   inputBlur: function (record: any): void {
     throw new Error('Function not implemented.');
-  }
+  },
 });
 
 const EditableRow: React.FC<EditableRowProps> = ({ index, ...props }) => {
@@ -57,14 +57,15 @@ const EditableCell: React.FC<EditableCellProps> = ({
 }) => {
   const form = useContext(EditableContext)!;
   let childNode = children;
-  const inputRef = useRef<any>()
+  const inputRef = useRef<any>();
   useEffect(() => {
     if (record) {
       form.setFieldsValue({ [dataIndex]: record[dataIndex] });
     }
   }, [record]);
   if (editable) {
-    const { inputType, prop } = editorConfig(record);
+    //@ts-ignore
+    const { inputType, prop, disable } = editorConfig(record);
     const { inputDoubleClick, inputBlur } = useContext(TableContext)!;
     if (inputType === EditType.INPUT) {
       childNode = (
@@ -72,29 +73,27 @@ const EditableCell: React.FC<EditableCellProps> = ({
           key={`col-input-${dataIndex}`}
           style={{ margin: 0 }}
           name={dataIndex}
-          rules={
-            [
-              {
-                required: true,
-                validator: (_props, value) => {
-                  var reg = new RegExp('^[a-zA-Z0-9_\u4e00-\u9fa5]+$');
-                  if (!value) {
-                    return Promise.reject(`请填写属性名称！`);
-                  }
-                  if (!reg.test(value)) {
-                    return Promise.reject('名称由中文、字母、数字、下划线组成。');
-                  } else {
-                    return Promise.resolve();
-                  }
-                },
+          rules={[
+            {
+              required: true,
+              validator: (_props, value) => {
+                var reg = new RegExp('^[a-zA-Z0-9_\u4e00-\u9fa5]+$');
+                if (!value) {
+                  return Promise.reject(`请填写属性名称！`);
+                }
+                if (!reg.test(value)) {
+                  return Promise.reject('名称由中文、字母、数字、下划线组成。');
+                } else {
+                  return Promise.resolve();
+                }
               },
-            ]
-          }
+            },
+          ]}
         >
-          {(record?.disable && record?.name)? (
+          {record?.disable && record?.name ? (
             <span
               style={{ height: '27px', backgroundColor: '#505156', color: '#fff', borderRadius: '8px', padding: '8px' }}
-              onClick={async() => {
+              onClick={async () => {
                 await inputDoubleClick(record);
                 await inputRef.current.focus();
               }}
@@ -102,14 +101,14 @@ const EditableCell: React.FC<EditableCellProps> = ({
               {record?.name} <EditOutlined />
             </span>
           ) : (
-            <Input ref={inputRef} {...prop} onBlur={() => inputBlur(record)} />
+            <Input ref={inputRef} {...prop} onBlur={() => inputBlur(record)} disabled={disable} />
           )}
         </Form.Item>
       );
     } else if (inputType === EditType.SELECT) {
       childNode = (
         <Form.Item style={{ margin: 0 }} name={dataIndex} key={`col-select-${dataIndex}`}>
-          <Select {...prop} options={prop?.options} />
+          <Select {...prop} options={prop?.options} disabled={disable} />
         </Form.Item>
       );
     } else {
@@ -122,44 +121,52 @@ const EditableCell: React.FC<EditableCellProps> = ({
     </td>
   );
 };
-export const EditTable: React.FC<{ columns: ConfigColumns[] ; dataSource:PropertyList[];onChange?:any;rowKey:string;inputDoubleClick?:any;inputBlur?:any;bordered?:boolean;showHeader?:boolean;rowSelection?:any;}> = memo(
-  ({ columns, dataSource, onChange, rowKey, inputDoubleClick, inputBlur, ...props }) => {
-    const data = dataSource;
-    const components = {
-      body: {
-        row: EditableRow,
-        cell: EditableCell,
-      },
+export const EditTable: React.FC<{
+  columns: ConfigColumns[];
+  dataSource: PropertyList[];
+  onChange?: any;
+  rowKey: string;
+  inputDoubleClick?: any;
+  inputBlur?: any;
+  bordered?: boolean;
+  showHeader?: boolean;
+  rowSelection?: any;
+}> = memo(({ columns, dataSource, onChange, rowKey, inputDoubleClick, inputBlur, ...props }) => {
+  const data = dataSource;
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+  const editColumns = columns.map(col => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record: any) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        editorConfig: col.editorConfig,
+      }),
     };
-    const editColumns = columns.map(col => {
-      if (!col.editable) {
-        return col;
-      }
-      return {
-        ...col,
-        onCell: (record: any) => ({
-          record,
-          editable: col.editable,
-          dataIndex: col.dataIndex,
-          title: col.title,
-          editorConfig: col.editorConfig,
-        }),
-      };
-    });
+  });
 
-    return (
-      <TableContext.Provider value={{ data, onChange, rowKey, inputDoubleClick, inputBlur }}>
-        <Table
-          columns={editColumns}
-          dataSource={data}
-          components={components}
-          rowKey={rowKey}
-          pagination={false}
-          {...props}
-          scroll={{y:300}}
-          // className={styles[`table-container`]}
-        />
-      </TableContext.Provider>
-    );
-  }
-);
+  return (
+    <TableContext.Provider value={{ data, onChange, rowKey, inputDoubleClick, inputBlur }}>
+      <Table
+        columns={editColumns}
+        dataSource={data}
+        components={components}
+        rowKey={rowKey}
+        pagination={false}
+        {...props}
+        scroll={{ y: 300 }}
+        // className={styles[`table-container`]}
+      />
+    </TableContext.Provider>
+  );
+});
