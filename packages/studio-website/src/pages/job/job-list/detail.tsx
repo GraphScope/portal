daf5d4b21d28d7dc9526fcebb411dbfa97f4c597
renyongdong <ryd01413914@alibaba-inc.com>
2024-03-05 10:34:17 +0800

    feat: job开始时间，结束时间排序，job详情通过url=jobid参数请求接口获取

--- packages/studio-website/src/pages/job/job-list/detail.tsx
+++ packages/studio-website/src/pages/job/job-list/detail.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Breadcrumb } from 'antd';
 import CodeMirror from '@uiw/react-codemirror';
 import { FormattedMessage } from 'react-intl';
-type IDetail = {
-  detailData: string;
-};
+import { getJobById } from '../service';
+import { getSearchParams } from '@/components/utils';
+type IDetail = {};
 const Detail: React.FunctionComponent<IDetail> = props => {
-  const { detailData } = props;
+  const { searchParams } = getSearchParams(window.location);
+  const JobId: string = searchParams.get('JobID') || '';
+  const [detailData, setDetailData] = useState('');
+  console.log(JobId);
+  useEffect(() => {
+    getJobByIdDetail();
+  }, []);
+  /** 获取详情job */
+  const getJobByIdDetail = async () => {
+    const res = await getJobById(JobId);
+    const { log } = res;
+    setDetailData(log);
+  };
   return (
     <div style={{ padding: '12px 24px' }}>
       <Breadcrumb
