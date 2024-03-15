06db24f4c126241d6fc04c4f684509dab34b548b
pomelo-nwu <czynwu@outlook.com>
2024-03-15 10:43:37 +0800

    feat: add global external

--- packages/studio-website/plugin.ts
+++ packages/studio-website/plugin.ts
@@ -20,6 +20,7 @@ export default (api: IApi) => {
       </style>
       <script>
        window.GS_ENGINE_TYPE="${params.engineType}";
+       window.GS_GREMLIN= {};
       </script>
       `,
     ]);
