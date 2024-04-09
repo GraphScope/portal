cf7a33f6cb6a67c5fbb5371e06553928cda89949
renyongdong <ryd01413914@alibaba-inc.com>
2024-04-09 10:33:53 +0800

    feat: split ts

--- packages/studio-website/src/components/utils/schema.tsx
+++ packages/studio-website/src/components/utils/schema.tsx
@@ -51,20 +51,28 @@ export type DeepRequired<T> = T extends (...args: any[]) => any
     ? { [K in keyof T]-?: DeepRequired<T[K]> }
     : T;
 
+export interface IProperty {
+  property_name: string;
+  property_type: { primitive_type: string };
+}
+export interface IEdge {
+  type_name: string;
+  properties: IProperty[];
+  vertex_type_pair_relations: { destination_vertex: string; source_vertex: string; relation: any }[];
+}
+
 export function transformSchema(originalSchema: DeepRequired<Schema>): TransformedSchema {
   const { vertex_types, edge_types } = originalSchema;
 
   const transformNode = (vertexType: DeepRequired<VertexType>): TransformedNode => ({
     label: vertexType.type_name,
-    properties: vertexType.properties.map(
-      (item: { property_name: string; property_type: { primitive_type: string } }) => {
-        const { property_name, property_type } = item;
-        return {
-          name: property_name,
-          type: property_type.primitive_type,
-        };
-      },
-    ),
+    properties: vertexType.properties.map((item: IProperty) => {
+      const { property_name, property_type } = item;
+      return {
+        name: property_name,
+        type: property_type.primitive_type,
+      };
+    }),
     primary: vertexType.primary_keys[0],
   });
 
@@ -72,29 +80,23 @@ export function transformSchema(originalSchema: DeepRequired<Schema>): Transform
   const edges: TransformedEdge[] = [];
   /** edge_types->undefined 报错 */
   if (edge_types) {
-    edge_types.forEach(
-      (edge: {
-        type_name: string;
-        properties: { property_name: string; property_type: { primitive_type: string } }[];
-        vertex_type_pair_relations: { destination_vertex: string; source_vertex: string; relation: any }[];
-      }) => {
-        const { type_name, properties, vertex_type_pair_relations } = edge;
-        vertex_type_pair_relations.forEach(c => {
-          const { destination_vertex, source_vertex, relation } = c;
-          edges.push({
-            label: type_name,
-            properties: (properties || []).map(({ property_name, property_type }) => ({
-              name: property_name,
-              type: property_type.primitive_type,
-            })),
+    edge_types.forEach((edge: IEdge) => {
+      const { type_name, properties, vertex_type_pair_relations } = edge;
+      vertex_type_pair_relations.forEach(c => {
+        const { destination_vertex, source_vertex, relation } = c;
+        edges.push({
+          label: type_name,
+          properties: (properties || []).map(({ property_name, property_type }) => ({
+            name: property_name,
+            type: property_type.primitive_type,
+          })),
 
-            source: source_vertex,
-            target: destination_vertex,
-            relation,
-          });
+          source: source_vertex,
+          target: destination_vertex,
+          relation,
         });
-      },
-    );
+      });
+    });
   }
 
   const transformedSchema: TransformedSchema = {
