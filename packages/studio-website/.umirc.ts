cfd0a22e3324a99d9cf6c5eec4a4450e0202721b
pomelo-nwu <czynwu@outlook.com>
2024-04-24 16:56:41 +0800

    feat: fix multiple instances of react

--- packages/studio-website/.umirc.ts
+++ packages/studio-website/.umirc.ts
@@ -24,7 +24,15 @@ export default defineConfig({
     '@antv/g2': 'G2',
     'node:os': 'commonjs2 node:os',
     // '@graphscope/_test_gremlin_': 'GS_GREMLIN',
-    // react: 'React', 'react-dom': 'ReactDOM'
+    // react: 'React',
+    // 'react-dom': 'ReactDOM',
+  },
+  mfsu: {
+    shared: {
+      react: {
+        singleton: true,
+      },
+    },
   },
 
   proxy: {
@@ -39,7 +47,7 @@ export default defineConfig({
 
   headScripts: [
     'https://gw.alipayobjects.com/os/lib/antv/g2/5.1.14/dist/g2.min.js',
-    //   'https://gw.alipayobjects.com/os/lib/react/18.2.0/umd/react.production.min.js',
-    //   'https://gw.alipayobjects.com/os/lib/react-dom/18.2.0/umd/react-dom.production.min.js',
+    // 'https://gw.alipayobjects.com/os/lib/react/18.2.0/umd/react.production.min.js',
+    // 'https://gw.alipayobjects.com/os/lib/react-dom/18.2.0/umd/react-dom.production.min.js',
   ],
 });
