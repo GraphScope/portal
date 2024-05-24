f1df235499e3e4dc6d36493cf425431d920c424a
renyongdong <ryd01413914@alibaba-inc.com>
2024-05-24 16:21:27 +0800

    feat: add ts

--- packages/studio-importor/src/components/PropertiesList/utils.ts
+++ packages/studio-importor/src/components/PropertiesList/utils.ts
@@ -1,76 +1,104 @@
-import React from 'react';
 import { uuid } from 'uuidv4';
+export interface Property {
+  key: string;
+  name: string;
+  type: string;
+  index: number;
+  primaryKey: boolean;
+  disable?: boolean;
+}
+export interface IState {
+  properties: Property[];
+}
+interface IStore {
+  handleAdd(state: IState): Property[];
+  handleDelete(state: IState & { selectedRowKeys: string[] }): Property[];
+  handleBlur(evt: { target: { value: string } }, record: { key: string }, state: IState): Property[];
+  handlePrimaryKey(record: { key: string }, state: IState): Property[];
+  handleDoubleClick(record: { key: string }, state: IState): Property[];
+  handleType(value: string, row: { key: string }, state: IState): Property[];
+}
 let index = 0;
-export const handleAdd = state => {
-  const newProperty = {
-    key: uuid(),
-    name: `property_${index++}`,
-    type: '',
-    primaryKey: false,
-    disable: true,
-  };
-  const properties = [...state.properties, newProperty];
-  return properties;
-};
-export const handleDelete = state => {
-  const properties = state.properties.filter(item => !state.selectedRowKeys.includes(item.key));
-  return properties;
-};
-/** input->blur */
-export const handleBlur = (evt, record, state) => {
-  const properties = state.properties.map(item => {
-    return {
-      ...item,
-      name: item.key === record.key ? evt.target.value : item.name,
+export default function useStore(): IStore {
+  const handleAdd = (state: IState): Property[] => {
+    const newProperty = {
+      key: uuid(),
+      name: `property_${index++}`,
+      type: '',
+      primaryKey: false,
       disable: true,
     };
-  });
-  return properties;
-};
-export const handlePrimaryKey = (record, state) => {
-  const properties = state.properties.map(item => {
-    if (item.key == record.key && item.primaryKey) {
+    const properties = [...state.properties, newProperty] as Property[];
+    return properties;
+  };
+  const handleDelete = (state: IState & { selectedRowKeys: string[] }): Property[] => {
+    const properties = state.properties.filter(item => !state.selectedRowKeys.includes(item.key));
+    return properties;
+  };
+  /** input->blur */
+  const handleBlur = (evt: { target: { value: string } }, record: { key: string }, state: IState): Property[] => {
+    const properties = state.properties.map(item => {
       return {
         ...item,
-        primaryKey: false,
+        name: item.key === record.key ? evt.target.value : item.name,
+        disable: true,
       };
-    } else {
-      if (item.key == record.key && !item.primaryKey) {
-        return {
-          ...item,
-          primaryKey: true,
-        };
-      } else {
+    });
+    return properties;
+  };
+  const handlePrimaryKey = (record: { key: string }, state: IState): Property[] => {
+    const properties = state.properties.map(item => {
+      if (item.key == record.key && item.primaryKey) {
         return {
           ...item,
           primaryKey: false,
         };
+      } else {
+        if (item.key == record.key && !item.primaryKey) {
+          return {
+            ...item,
+            primaryKey: true,
+          };
+        } else {
+          return {
+            ...item,
+            primaryKey: false,
+          };
+        }
       }
-    }
-  });
-  return properties;
-};
+    });
+    return properties;
+  };
 
-/** 双击编辑 */
-export const handleDoubleClick = (record, state) => {
-  const properties = state.properties.map(item => {
-    return {
-      ...item,
-      disable: item.key !== record.key,
-    };
-  });
-  return properties;
-};
-
-export const handleType = (value, row, state) => {
-  const properties = state.properties.map(item => {
-    if (item.key === row.key) {
+  /** 双击编辑 */
+  const handleDoubleClick = (record: { key: string }, state: IState): Property[] => {
+    const properties = state.properties.map(item => {
       return {
         ...item,
-        type: value,
+        disable: item.key !== record.key,
       };
-    }
-    return item;
-  });
-  return properties;
-};
+    });
+    return properties;
+  };
+
+  const handleType = (value: string, row: { key: string }, state: IState): Property[] => {
+    const properties = state.properties.map(item => {
+      if (item.key === row.key) {
+        return {
+          ...item,
+          type: value,
+        };
+      }
+      return item;
+    });
+    return properties;
+  };
+  return {
+    handleAdd,
+    handleDelete,
+    handleBlur,
+    handlePrimaryKey,
+    handleDoubleClick,
+    handleType,
+  };
+}
