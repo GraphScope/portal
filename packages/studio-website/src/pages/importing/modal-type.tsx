0174bcfd71cc6997954af7e961adde30409c8b87
renyongdong <ryd01413914@alibaba-inc.com>
2024-06-14 09:22:41 +0800

    feat: add loading config modal

---
+++ packages/studio-website/src/pages/importing/modal-type.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import type { FormProps } from 'antd';
+import { Button, Select, Form, Input } from 'antd';
+
+export type FieldType = {
+  type?: string;
+  delimiter?: string;
+  import_option?: string;
+};
+
+interface IModalType {
+  handleFinish: (values: FieldType) => void;
+}
+const ModalType: React.FC<IModalType> = props => {
+  const { handleFinish } = props;
+  return (
+    <Form name="modal_type" style={{ marginTop: '24px' }} labelCol={{ span: 5 }} onFinish={handleFinish}>
+      <Form.Item<FieldType> label="Type" name="type">
+        <Select
+          allowClear
+          options={[
+            { label: 'csv', value: 'csv' },
+            { label: 'odps', value: 'odps' },
+          ]}
+        />
+      </Form.Item>
+
+      <Form.Item<FieldType> label="delimiter" name="delimiter">
+        <Select
+          allowClear
+          options={[
+            { label: '|', value: '|' },
+            { label: ',', value: ',' },
+          ]}
+        />
+      </Form.Item>
+      <Form.Item<FieldType> label="import_option" name="import_option">
+        <Select
+          allowClear
+          options={[
+            { label: 'overwrite', value: 'overwrite' },
+            { label: 'init', value: 'init' },
+          ]}
+        />
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 10, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default ModalType;
--- packages/studio-website/src/pages/importing/start-importing.tsx
+++ packages/studio-website/src/pages/importing/start-importing.tsx
@@ -1,19 +1,26 @@
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import * as React from 'react';
 import { useContext } from '../../layouts/useContext';
 import { useContext as useImporting } from '@graphscope/studio-importor';
 import { submitDataloadingJob, bindDatasourceInBatch } from './services';
-
+import ModalType from './modal-type';
+import type { FieldType } from './modal-type';
 interface StartImportingProps {
   onClick?: () => void;
 }
-
+const { useState } = React;
 const StartImporting: React.FunctionComponent<StartImportingProps> = props => {
   const { store } = useContext();
   const { store: importingStore } = useImporting();
   const { appMode, nodes, edges } = importingStore;
   const { graphId } = store;
-  const handleClick = async () => {
+  const [state, updateState] = useState({
+    open: false,
+  });
+  const { open } = state;
+  const handleClick = async (values: FieldType) => {
+    /** loading config */
+    console.log(values);
     //先绑定数据
     const bindStatus = await bindDatasourceInBatch(graphId, {
       nodes: nodes.map(item => item.data),
@@ -25,14 +32,33 @@ const StartImporting: React.FunctionComponent<StartImportingProps> = props => {
       edges,
     });
     console.log('res', res);
+    updateState(preset => {
+      return {
+        ...preset,
+        open: false,
+      };
+    });
   };
 
   if (appMode === 'DATA_IMPORTING') {
     return (
       <>
-        <Button type="primary" onClick={handleClick}>
+        <Button
+          type="primary"
+          onClick={() => {
+            updateState(preset => {
+              return {
+                ...preset,
+                open: true,
+              };
+            });
+          }}
+        >
           Start importing
         </Button>
+        <Modal title="Loading Config" open={open} footer={null} closable={false}>
+          <ModalType handleFinish={handleClick} />
+        </Modal>
       </>
     );
   }
