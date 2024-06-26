9684f1a946c87b54b507af5070d0ba7284fb0fe5
renyongdong <ryd01413914@alibaba-inc.com>
2024-06-26 09:38:10 +0800

    feat: update bug

---
+++ packages/studio-components/src/Provider/const.ts
@@ -0,0 +1,34 @@
+/** components 基础配置 */
+export const components = {
+  Menu: {
+    itemBg: 'rgba(255, 255, 255, 0)',
+    subMenuItemBg: 'rgba(255, 255, 255, 0)',
+    iconMarginInlineEnd: 14,
+    itemMarginInline: 4,
+    iconSize: 14,
+    collapsedWidth: 60,
+  },
+  Typography: {
+    titleMarginBottom: '0.2em',
+    titleMarginTop: '0.8em',
+  },
+  Table: {
+    cellPaddingBlock: 4, //	单元格纵向内间距
+    cellPaddingInline: 8, //单元格横向内间距（默认大尺寸）
+  },
+  Pagination: {
+    itemSize: 20,
+  },
+  Result: {
+    iconFontSize: 62,
+    titleFontSize: 20,
+    colorError: '#00000073',
+  },
+};
+
+/** token 基础配置 */
+export const token = {
+  colorBorder: '#303030',
+  colorBgBase: 'rgba(12,12,12,1)',
+  colorBgLayout: 'rgba(43,43,43,1)',
+};
