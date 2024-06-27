0f550825f3514411fbbb2be3d69fce34fee1905d
renyongdong <ryd01413914@alibaba-inc.com>
2024-06-27 17:01:06 +0800

    feat: optimize code

--- packages/studio-importor/src/app/properties-editor/properties-schema/extra-component.tsx
+++ packages/studio-importor/src/app/properties-editor/properties-schema/extra-component.tsx
@@ -11,34 +11,34 @@ interface IExtraComponentProps {
 }
 
 const ExtraComponent: React.FC<IExtraComponentProps> = ({ appMode, isEditable, type, properties }) => {
+  const TooltipId = getTitle({ appMode, isEditable, type, properties });
+  return (
+    <>
+      {TooltipId && (
+        <Tooltip title={<FormattedMessage id={`${TooltipId}`} />}>
+          <Button type="text" icon={<WarnIcon />} />
+        </Tooltip>
+      )}
+    </>
+  );
+};
+/** 获取不同状态下title */
+export function getTitle({ appMode, isEditable, type, properties }: IExtraComponentProps): string | null {
   if (appMode !== 'DATA_IMPORTING' && isEditable) {
     if (type === 'node') {
       if (!properties || properties.length === 0) {
-        return (
-          <Tooltip title={<FormattedMessage id="A vertex must have at least one property." />}>
-            <Button type="text" icon={<WarnIcon />} />
-          </Tooltip>
-        );
+        return 'A vertex must have at least one property.';
       }
       const hasPrimaryKey = properties.some(({ primaryKey }) => primaryKey);
       if (!hasPrimaryKey) {
-        return (
-          <Tooltip title={<FormattedMessage id="A vertex must have a primary key." />}>
-            <Button type="text" icon={<WarnIcon />} />
-          </Tooltip>
-        );
+        return 'A vertex must have a primary key.';
       }
     } else if (type === 'edge') {
       if (properties?.length !== 1) {
-        return (
-          <Tooltip title={<FormattedMessage id="An edge can only have one property." />}>
-            <Button type="text" icon={<WarnIcon />} />
-          </Tooltip>
-        );
+        return 'An edge can only have one property.';
       }
     }
   }
   return null;
-};
-
+}
 export default ExtraComponent;
