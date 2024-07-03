970990162eaea84a7b0f3b4f2eb47f188ead25a6
pomelo-nwu <czynwu@outlook.com>
2024-07-03 11:43:39 +0800

    feat: pref validate properties logic

--- packages/studio-importor/src/index.tsx
+++ packages/studio-importor/src/index.tsx
@@ -1,7 +1,7 @@
 import ImportApp from './app';
 
 export { useContext } from './app/useContext';
-export { getTooltipTitle } from './app/properties-editor/properties-schema/validate-info';
+export { validateProperties } from './app/properties-editor/properties-schema/validate-info';
 export type { ISchemaEdge, ISchemaNode, ISchemaOptions } from './app/typing';
 export type { Property } from '@graphscope/studio-components';
 export default ImportApp;
