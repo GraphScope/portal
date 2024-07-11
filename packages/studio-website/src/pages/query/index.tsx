886de2a95a85cfad216eeb87f2c50f456377f79e
renyongdong <ryd01413914@alibaba-inc.com>
2024-07-11 10:53:26 +0800

    feat: add loading

--- packages/studio-website/src/pages/query/index.tsx
+++ packages/studio-website/src/pages/query/index.tsx
@@ -1,10 +1,20 @@
 import React, { lazy, Suspense } from 'react';
-
+import { Flex, Spin, Space } from 'antd';
 const StudioQuery = lazy(() => import('./app'));
-
+const boxStyle: React.CSSProperties = {
+  width: '100%',
+  height: '100%',
+};
 const Loading = () => {
   console.log('QueryModule 1 ');
-  return <div>Loading...</div>;
+  return (
+    <Flex style={boxStyle} vertical justify="center" align="center">
+      <Space.Compact direction="vertical">
+        <Spin size="large" />
+        <p style={{ marginLeft: '24px' }}>Loading...</p>
+      </Space.Compact>
+    </Flex>
+  );
 };
 const QueryModule = () => {
   return (
