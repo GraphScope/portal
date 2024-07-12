4afa0dfc8b6446cbf422e93d941ad1488b4b33a3
renyongdong <ryd01413914@alibaba-inc.com>
2024-07-12 17:25:13 +0800

    feat: loading

--- packages/studio-website/src/pages/query/index.tsx
+++ packages/studio-website/src/pages/query/index.tsx
@@ -1,25 +1,12 @@
 import React, { lazy, Suspense } from 'react';
-import { Flex, Spin, Space } from 'antd';
+import LoadingProgress from './loading-progress';
+
 const StudioQuery = lazy(() => import('./app'));
-const boxStyle: React.CSSProperties = {
-  width: '100%',
-  height: '100%',
-};
-const Loading = () => {
-  console.log('QueryModule 1 ');
-  return (
-    <Flex style={boxStyle} vertical justify="center" align="center">
-      <Space.Compact direction="vertical">
-        <Spin size="large" />
-        <p style={{ marginLeft: '24px' }}>Loading...</p>
-      </Space.Compact>
-    </Flex>
-  );
-};
+
 const QueryModule = () => {
   return (
     <>
-      <Suspense fallback={<Loading />}>
+      <Suspense fallback={<LoadingProgress />}>
         <StudioQuery />
       </Suspense>
     </>
