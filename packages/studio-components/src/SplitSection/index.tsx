afc84966b7d096e1bb49d5e2f97b0c1b3fd64f00
renyongdong <ryd01413914@alibaba-inc.com>
2024-07-12 10:08:12 +0800

    feat: update extension

--- packages/studio-components/src/SplitSection/index.tsx
+++ packages/studio-components/src/SplitSection/index.tsx
@@ -4,27 +4,27 @@ import { Modal, Result, Button, Col, Row, Flex, Divider } from 'antd';
 interface ISplitSectionProps {
   splitText?: string;
   span?: number;
-  splitDivider?: number;
+  splitSpan?: number;
   leftSide: React.ReactNode;
   rightSide?: React.ReactNode;
 }
 
 const SplitSection: React.FunctionComponent<ISplitSectionProps> = props => {
-  const { splitText = 'OR', span = 11, splitDivider = 2, leftSide, rightSide } = props;
+  const { splitText = 'OR', span = 11, splitSpan = 2, leftSide, rightSide } = props;
   if (!rightSide) {
     return <>{leftSide}</>;
   }
   return (
     <Row>
       <Col span={span}>{leftSide}</Col>
-      <Col span={splitDivider}>
+      <Col span={splitSpan}>
         <Flex vertical style={{ height: '100%', alignItems: 'center' }}>
           <Divider type="vertical" style={{ flex: 1 }} />
           {splitText}
           <Divider type="vertical" style={{ flex: 1 }} />
         </Flex>
       </Col>
-      <Col span={24 - span - splitDivider}>{rightSide}</Col>
+      <Col span={24 - span - splitSpan}>{rightSide}</Col>
     </Row>
   );
 };
--- packages/studio-website/src/pages/extension/create-plugins.tsx
+++ packages/studio-website/src/pages/extension/create-plugins.tsx
@@ -9,7 +9,7 @@ import UploadFiles from './upload-files';
 import Section from '@/components/section';
 
 import { createProcedure, updateProcedure, listGraphs, getProcedure } from './service';
-const { searchParamOf } = Utils;
+const { searchParamOf, getUrlParams } = Utils;
 
 type FieldType = {
   name: string;
@@ -24,10 +24,9 @@ const TYPEOPTION = [
   // { label: 'Cpp', value: 'cpp' },
 ];
 const CreatePlugins: React.FC = props => {
+  console.log(getUrlParams());
+  const { graph_id, procedure_id } = getUrlParams();
   const [form] = Form.useForm();
-  const graph_id = searchParamOf('graph_id') || '';
-  const procedure_id = searchParamOf('procedure_id') || '';
-
   const [state, updateState] = useState<{
     editCode: string;
     instanceOption: { label: string; value: string }[];
@@ -40,19 +39,20 @@ const CreatePlugins: React.FC = props => {
   const { editCode, instanceOption, isEdit } = state;
   const { pluginBorder } = useThemeContainer();
   /** 获取插件某条数据 */
-  const getProcedures = useCallback(async (graph_id: string, procedure_id: string) => {
+  const getProcedures = async (graph_id: string, procedure_id: string) => {
     const res = await getProcedure(graph_id, procedure_id);
-    const { query } = res as { query: string };
+    const { query } = res;
     form.setFieldsValue(res);
-    updateState(preset => ({ ...preset, editCode: query, isEdit: true }));
-  }, []);
+    updateState(preset => {
+      return { ...preset, editCode: query, isEdit: true };
+    });
+  };
 
   /** 创建插件 */
   const handleSubmit = useCallback(async () => {
     /** bound_graph 是 graph_ids */
     const { name, bound_graph, type, description } = form.getFieldsValue();
-    console.log(editCode);
-    const data = { name, description, type, query: editCode };
+    const data = { name: name.trim(), description, type, query: editCode };
     if (graph_id) {
       /** 修改插件 */
       await updateProcedure(bound_graph, procedure_id, data);
@@ -64,17 +64,82 @@ const CreatePlugins: React.FC = props => {
   }, [editCode, form.getFieldsValue()]);
   /** 获取editcode */
   const onCodeMirrorChange = useCallback((val: string) => {
-    updateState(preset => ({ ...preset, editCode: val }));
+    updateState(preset => {
+      return { ...preset, editCode: val };
+    });
   }, []);
   useEffect(() => {
     form.setFieldsValue({ type: 'cypher' });
     listGraphs().then(res => {
-      updateState(preset => ({ ...preset, instanceOption: res }));
+      updateState(preset => {
+        return { ...preset, instanceOption: res };
+      });
     });
     if (graph_id && procedure_id) {
       getProcedures(graph_id, procedure_id);
     }
   }, []);
+  const LeftSide = () => {
+    return (
+      <>
+        {!editCode ? (
+          <UploadFiles disabled={isEdit} handleChange={onCodeMirrorChange} />
+        ) : (
+          <div
+            style={{
+              overflow: 'scroll',
+              border: `1px solid ${pluginBorder}`,
+              borderRadius: '8px',
+            }}
+          >
+            <CodeMirror
+              height="240px"
+              value={editCode}
+              onChange={e => onCodeMirrorChange(e)}
+              theme={useEditorTheme(isEdit)}
+              readOnly={isEdit}
+            />
+          </div>
+        )}
+      </>
+    );
+  };
+  const RightSide = () => {
+    return (
+      <Form name="basic" labelCol={{ span: 3 }} wrapperCol={{ span: 21 }} form={form} onFinish={handleSubmit}>
+        <Form.Item<FieldType>
+          label={<FormattedMessage id="Name" />}
+          name="name"
+          rules={[{ required: true, message: 'Please input your Graph name!', pattern: /^[^\s]*$/ }]}
+        >
+          <Input disabled={isEdit} />
+        </Form.Item>
+
+        <Form.Item<FieldType>
+          label={<FormattedMessage id="Plugin Type" />}
+          name="type"
+          rules={[{ required: true, message: 'Please input your Plugin Type!' }]}
+        >
+          <Select options={TYPEOPTION} disabled={isEdit} />
+        </Form.Item>
+        <Form.Item<FieldType>
+          label={<FormattedMessage id="Binding graph" />}
+          name="bound_graph"
+          rules={[{ required: true, message: 'Please input your Graph Instance!' }]}
+        >
+          <Select options={instanceOption} disabled={isEdit} />
+        </Form.Item>
+        <Form.Item<FieldType> label={<FormattedMessage id="Description" />} name="description">
+          <Input />
+        </Form.Item>
+        <Form.Item style={{ display: 'flex', justifyContent: 'end', marginBottom: '0px' }}>
+          <Button type="primary" htmlType="submit">
+            <FormattedMessage id="Create Plugin" />
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  };
   return (
     <Section
       breadcrumb={[
@@ -87,66 +152,7 @@ const CreatePlugins: React.FC = props => {
       ]}
       desc="Expand its functionality or offer solutions that are finely tuned to specific needs."
     >
-      <Flex vertical gap={12}>
-        <UploadFiles disabled={isEdit} handleChange={onCodeMirrorChange} />
-        <Form name="basic" labelCol={{ span: 3 }} wrapperCol={{ span: 21 }} form={form} onFinish={handleSubmit}>
-          <Form.Item<FieldType>
-            style={{ marginBottom: '12px' }}
-            label={<FormattedMessage id="Name" />}
-            name="name"
-            rules={[{ required: true, message: 'Please input your Graph name!' }]}
-          >
-            <Input disabled={isEdit} />
-          </Form.Item>
-
-          <Form.Item<FieldType>
-            style={{ marginBottom: '12px' }}
-            label={<FormattedMessage id="Plugin Type" />}
-            name="type"
-            rules={[{ required: true, message: 'Please input your Plugin Type!' }]}
-          >
-            <Select options={TYPEOPTION} disabled={isEdit} />
-          </Form.Item>
-          <Form.Item<FieldType>
-            style={{ marginBottom: '12px' }}
-            label={<FormattedMessage id="Binding graph" />}
-            name="bound_graph"
-            rules={[{ required: true, message: 'Please input your Graph Instance!' }]}
-          >
-            <Select options={instanceOption} disabled={isEdit} />
-          </Form.Item>
-          <Form.Item<FieldType>
-            style={{ marginBottom: '12px' }}
-            label={<FormattedMessage id="Description" />}
-            name="description"
-            rules={[{ required: true, message: 'Please input your Description!' }]}
-          >
-            <Input />
-          </Form.Item>
-          <Form.Item<FieldType> label={<FormattedMessage id="Edit code" />} name="query">
-            <div
-              style={{
-                overflow: 'scroll',
-                border: `1px solid ${pluginBorder}`,
-                borderRadius: '8px',
-              }}
-            >
-              <CodeMirror
-                height="240px"
-                value={editCode}
-                onChange={e => onCodeMirrorChange(e)}
-                theme={useEditorTheme(isEdit)}
-                readOnly={isEdit}
-              />
-            </div>
-          </Form.Item>
-          <Form.Item style={{ display: 'flex', justifyContent: 'end', marginBottom: '0px' }}>
-            <Button type="primary" htmlType="submit">
-              <FormattedMessage id="Create Plugin" />
-            </Button>
-          </Form.Item>
-        </Form>
-      </Flex>
+      <SplitSection splitText="" span={8} splitSpan={1} leftSide={<LeftSide />} rightSide={<RightSide />} />
     </Section>
   );
 };
