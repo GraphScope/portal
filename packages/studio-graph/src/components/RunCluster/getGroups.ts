9dbc71f3160a7c0b929c8427d946a67821943a3f
pomelo-nwu <czynwu@outlook.com>
2024-07-31 13:44:01 +0800

    feat: pref combo radius caculate

--- packages/studio-graph/src/components/RunCluster/getGroups.ts
+++ packages/studio-graph/src/components/RunCluster/getGroups.ts
@@ -1,7 +1,7 @@
 import { pack, hierarchy } from 'd3-hierarchy';
 import { Utils } from '@graphscope/studio-components';
 import { handleStyle } from '../../graph/handleStyle';
- 
+
 interface NodeData {
   children: NodeData[];
   name: string;
@@ -11,36 +11,30 @@ export const getGroups = (nodes, { width, height, screen2GraphCoords, nodeStyle 
     return node.label;
   });
 
-  const root:NodeData = { name: 'root', children: [] };
+  const root: NodeData = { name: 'root', children: [] };
 
   for (const [label, nodes] of Object.entries(groupedData)) {
     root.children.push({ name: label, children: nodes as NodeData[] });
   }
-  const rootNode = hierarchy<NodeData>(root)
-    // .sum(d => {
-    //   return d.size || 9;
-    // })
-    .sort((a, b) => {
-      try {
-        // 如果 label 是数字，则按照数字大小排序
-        return Number(a.data.name) - Number(b.data.name);
-      } catch (error) {
-        return 0;
-      }
-    });
-
-  // Circle Packing 布局，计算分组中心
+  const rootNode = hierarchy<NodeData>(root).sort((a, b) => {
+    try {
+      // 如果 label 是数字，则按照数字大小排序
+      return Number(a.data.name) - Number(b.data.name);
+    } catch (error) {
+      return 0;
+    }
+  });
+  const padding = 20;
   const packLayput = pack<NodeData>()
     .size([width, height])
-    .padding(20)
+    .padding(padding)
     .radius(d => {
       const { size } = handleStyle(d.data, nodeStyle, 'node');
-      const R = Math.round(Math.sqrt(Math.max(0, size)) * 4 + 2);
-      return R / 2 || 1;
+      const R = Math.round(Math.sqrt(Math.max(0, size)) * 4 + 2 - padding / 2);
+      return R;
     });
 
   packLayput(rootNode);
- 
 
   // 绘制分组节点
   const groups = (rootNode.children || []).map(d => {
