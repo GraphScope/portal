f308ad5e3a90bdab8981ff0ed1b4171ad8e2c4b0
卜庆鑫 <buqingxin@xiaomi.com>
2024-08-20 10:39:44 +0800

    feat: propover component in editor

--- packages/studio-graph-editor/src/elements/node-types/graph-node.tsx
+++ packages/studio-graph-editor/src/elements/node-types/graph-node.tsx
@@ -1,12 +1,13 @@
-import React, { memo, MouseEventHandler, useState } from 'react';
+import React, { ChangeEvent, memo, MouseEventHandler, useEffect, useRef, useState } from 'react';
 import { Handle, NodeProps, Position } from 'reactflow';
-import { Tag, Button, theme } from 'antd';
+import { Tag, Button, theme, Popover, Input } from 'antd';
 import { CheckCircleOutlined, CheckOutlined } from '@ant-design/icons';
-import { EditableText, useThemeContainer, useSection } from '@graphscope/studio-components';
+import { EditableText, useThemeContainer, useSection, PropertiesList, Property } from '@graphscope/studio-components';
 const { useToken } = theme;
 import { useContext } from '../../canvas/useContext';
 import { LazyLoad } from '../middle-component/lazy-load';
 import { GraphContext, useGraphContext } from '../..';
+import { Option } from '@graphscope/studio-components/lib/PropertiesList';
 
 const R = 50;
 const HALO_LINE_WIDTH = 16;
@@ -40,6 +41,8 @@ const GraphNode = (props: NodeProps) => {
     isHover: false,
     contentEditable: false,
   });
+  const [visible, setVisible] = useState<boolean>(false);
+  const nodeRef = useRef<HTMLDivElement>(null);
 
   const onMouseEnter = () => {
     if (isConnectable) {
@@ -63,9 +66,13 @@ const GraphNode = (props: NodeProps) => {
       };
     });
   };
+
+  const currentNode = store.nodes.find(node => node.id === id);
+
   const hanleChangeLabel = value => {
     updateStore(draft => {
       //@ts-ignore
+      // const match = draft.nodes.find(node => node.id === id);
       const match = draft.nodes.find(node => node.id === id);
       if (match) {
         //@ts-ignore
@@ -73,6 +80,7 @@ const GraphNode = (props: NodeProps) => {
       }
     });
   };
+
   const onClick: MouseEventHandler<HTMLDivElement> = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     updateStore(draft => {
       draft.currentType = 'nodes';
@@ -82,6 +90,7 @@ const GraphNode = (props: NodeProps) => {
     toggleRightSide(false);
     toggleLeftSide(true);
     onNodeClick && onNodeClick(data, event);
+    setVisible(true);
   };
   const haloStyle = !isConnectable
     ? {
@@ -101,65 +110,133 @@ const GraphNode = (props: NodeProps) => {
     }
     return isSelected ? `4px solid ${theme.primaryColor}` : '2px solid #000';
   };
+
+  // 监听用户 click position
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (nodeRef.current && !nodeRef.current.contains(event.target)) setVisible(false);
+    }
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [nodeRef]);
+
+  const handleLabelChange = (event: ChangeEvent) => {
+    // @ts-ignore
+
+    updateStore(draft => {
+      //@ts-ignore
+      const match = draft.nodes.find(node => node.id === id);
+      if (match) {
+        //@ts-ignore
+        match.data.label = event.target.value;
+      }
+    });
+  };
+
+  const handlePropertiesListChange = (value: Property[]) => {
+    console.log(value);
+  };
+
+  const typeColumn: Option[] = [
+    {
+      label: 'string',
+      value: 'string',
+    },
+    {
+      label: 'number',
+      value: 'number',
+    },
+  ];
+  const handleChange = list => {
+    console.log('properties', list);
+  };
+
   return (
-    <div
-      data-nodeid={id}
-      style={{ boxSizing: 'border-box', width: '100px', height: '100px' }}
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-      onClick={onClick}
-    >
-      <Handle
-        type="target"
-        position={Position.Left}
-        id="left"
-        style={{
-          background: 'transparent',
-          position: 'relative',
-          border: 'none',
-        }}
-      />
-
-      <Handle type="source" position={Position.Right} id="right" style={haloStyle}></Handle>
-
+    <div ref={nodeRef}>
       <div
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          border: getBorder(),
-          backgroundColor: isDark ? '#161616' : '#fafafa',
-          borderRadius: '50%',
-          height: NODE_WIDTH,
-          width: NODE_WIDTH,
-          boxSizing: 'border-box',
-          zIndex: 0,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: '20px',
-        }}
         data-nodeid={id}
+        style={{ boxSizing: 'border-box', width: '100px', height: '100px' }}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        onClick={onClick}
       >
-        {filelocation && (
-          <div
-            style={{
-              position: 'absolute',
-              top: '0px',
-              right: '-16px',
-            }}
-          >
-            <CheckOutlined style={{ color: token.colorSuccessActive, fontSize: 20 }} />
-          </div>
-        )}
-        <EditableText
-          id={id}
-          text={label}
-          onTextChange={hanleChangeLabel}
-          disabled={disabled}
-          style={{ color: isDark ? '#D7D7D7' : '#000' }}
+        <Handle
+          type="target"
+          position={Position.Left}
+          id="left"
+          style={{
+            background: 'transparent',
+            position: 'relative',
+            border: 'none',
+          }}
         />
+
+        <Handle type="source" position={Position.Right} id="right" style={haloStyle}></Handle>
+
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            border: getBorder(),
+            backgroundColor: isDark ? '#161616' : '#fafafa',
+            borderRadius: '50%',
+            height: NODE_WIDTH,
+            width: NODE_WIDTH,
+            boxSizing: 'border-box',
+            zIndex: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            fontSize: '20px',
+          }}
+          data-nodeid={id}
+        >
+          {filelocation && (
+            <div
+              style={{
+                position: 'absolute',
+                top: '0px',
+                right: '-16px',
+              }}
+            >
+              <CheckOutlined style={{ color: token.colorSuccessActive, fontSize: 20 }} />
+            </div>
+          )}
+          <EditableText
+            id={id}
+            text={currentNode.data.label}
+            onTextChange={hanleChangeLabel}
+            disabled={disabled}
+            style={{ color: isDark ? '#D7D7D7' : '#000' }}
+          />
+        </div>
       </div>
+      {visible && (
+        <div
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '120px',
+            backgroundColor: 'white',
+            width: '400px',
+            minHeight: '300px',
+            border: '1px solid #D7D7D7',
+            borderRadius: '12px',
+            boxShadow: '0 0 8px #f6f6f6',
+            transform: 'translateY(-50%)',
+            padding: '20px',
+          }}
+        >
+          <span>label</span>
+          <Input value={currentNode.data.label} onChange={handleLabelChange}></Input>
+          {/* <PropertiesList typeColumn={{ options: typeColumn }} onChange={handleChange}></PropertiesList> */}
+        </div>
+      )}
     </div>
   );
 };
