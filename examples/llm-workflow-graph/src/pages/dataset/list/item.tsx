1fb44fee3938083eb8fde55307585a3940709658
pomelo-nwu <czynwu@outlook.com>
2024-08-24 20:41:28 +0800

    feat: update style

--- examples/llm-workflow-graph/src/pages/dataset/list/item.tsx
+++ examples/llm-workflow-graph/src/pages/dataset/list/item.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, Flex, Space, Button, theme, Divider } from 'antd';
+import { Typography, Flex, Space, Button, theme, Divider, Tooltip } from 'antd';
 
 import { useNavigate } from 'react-router-dom';
 
@@ -7,6 +7,7 @@ import { GraphList, GraphSchema } from '../../components';
 import type { IDataset } from '../typing';
 import GraphView from '../embed/view';
 import { downloadDataset } from '../service';
+import { SettingOutlined, FileZipOutlined, DeploymentUnitOutlined } from '@ant-design/icons';
 const { useToken } = theme;
 
 const styles: Record<string, React.CSSProperties> = {
@@ -36,31 +37,44 @@ const List: React.FunctionComponent<IDataset> = props => {
   return (
     <Flex vertical flex={1} style={styles.container} gap={8}>
       <Flex justify="space-between" align="center" style={styles.card}>
-        <Typography.Text type="secondary">当前上传了 255 份 PDF 文件，暂未解析实体与关系</Typography.Text>
+        <Typography.Text type="secondary">Currently, 255 PDF files have been uploaded</Typography.Text>
         <Space>
-          <Button
-            onClick={() => {
-              navigation(`/dataset/embed?id=${id}`);
-            }}
-            type={status === 'WAITING_EMBEDDING' ? 'primary' : 'default'}
-          >
-            Embed Graph
-          </Button>
-          <Divider style={{ width: '40px' }} />
+          <Tooltip title="Embed the graph and inform the LLM how to extract your unstructured data.">
+            <Button
+              icon={<DeploymentUnitOutlined />}
+              onClick={() => {
+                navigation(`/dataset/embed?id=${id}`);
+              }}
+              type={status === 'WAITING_EMBEDDING' ? 'primary' : 'default'}
+            >
+              STEP 1
+            </Button>
+          </Tooltip>
 
-          <Button
-            onClick={() => {
-              navigation(`/dataset/extract?id=${id}`);
-            }}
-            disabled={status === 'WAITING_EMBEDDING'}
-            type={status === 'WAITING_EXTRACT' ? 'primary' : 'default'}
-          >
-            Extract
-          </Button>
           <Divider style={{ width: '40px' }} />
-          <Button disabled={!summarized} type={summarized ? 'primary' : 'default'} onClick={downloadDataset}>
-            Download
-          </Button>
+          <Tooltip title="Configure the server parameters and start extracting entities and relationships.">
+            <Button
+              onClick={() => {
+                navigation(`/dataset/extract?id=${id}`);
+              }}
+              icon={<SettingOutlined />}
+              disabled={status === 'WAITING_EMBEDDING'}
+              type={status === 'WAITING_EXTRACT' ? 'primary' : 'default'}
+            >
+              STEP 2
+            </Button>
+          </Tooltip>
+          <Divider style={{ width: '40px' }} />
+          <Tooltip title="Download the dataset and import it into the graph database for further analysis.">
+            <Button
+              icon={<FileZipOutlined />}
+              disabled={!summarized}
+              type={summarized ? 'primary' : 'default'}
+              onClick={downloadDataset}
+            >
+              STEP 3
+            </Button>
+          </Tooltip>
         </Space>
       </Flex>
       <Flex justify="space-between" gap={8}>
