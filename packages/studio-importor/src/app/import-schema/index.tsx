11d77b51e4622eb06d3dfe2d121a026f045c8259
pomelo-nwu <czynwu@outlook.com>
2024-09-20 14:38:31 +0800

    feat: add shortcut for modeling

--- packages/studio-importor/src/app/import-schema/index.tsx
+++ packages/studio-importor/src/app/import-schema/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Button, Modal, Segmented } from 'antd';
+import { Button, Modal, Segmented, Space, Tooltip } from 'antd';
 import { UngroupOutlined } from '@ant-design/icons';
 import ImportFromCSV from './import-from-csv';
 import ImportFromSQL from './import-from-sql';
 import ImportFromYAML from './import-from-yaml';
-import { SegmentedTabs } from '@graphscope/studio-components';
+import { SegmentedTabs, Icons } from '@graphscope/studio-components';
 import type { SegmentedTabsProps } from '@graphscope/studio-components';
+
 interface IImportSchemaProps {
   style?: React.CSSProperties;
   displayType?: 'model' | 'panel';
@@ -13,15 +14,15 @@ interface IImportSchemaProps {
 
 const items: SegmentedTabsProps['items'] = [
   {
-    label: 'File',
-    key: 'File',
+    label: 'CSV',
+    key: 'CSV',
     children: <ImportFromCSV />,
   },
-  // {
-  //   label: 'SQL DDL',
-  //   key: 'SQL',
-  //   children: <ImportFromSQL />,
-  // },
+  {
+    label: 'YAML',
+    key: 'SQL',
+    children: <ImportFromYAML icon={<Icons.FileYaml style={{ fontSize: '50px' }} />} />,
+  },
 ];
 
 const ImportSchema: React.FunctionComponent<IImportSchemaProps> = props => {
@@ -44,12 +45,23 @@ const ImportSchema: React.FunctionComponent<IImportSchemaProps> = props => {
   };
   if (displayType === 'model') {
     return (
-      <>
-        <Button type="text" onClick={handleClick} style={style} icon={<UngroupOutlined />}></Button>
-        <Modal title="import schema" open={visible} onOk={handleClose} onCancel={handleClose}>
-          <Segmented options={['CSV', 'SQL DDL']} block></Segmented>
+      <Space>
+        <Tooltip title="Shortcut: parse files into a graph model" placement="left">
+          <Button type="text" onClick={handleClick} style={style} icon={<Icons.FileYaml />}></Button>
+        </Tooltip>
+
+        <Modal
+          title="Parse files into a graph model"
+          open={visible}
+          onOk={handleClose}
+          onCancel={handleClose}
+          footer={null}
+        >
+          <div style={{ height: '60vh' }}>
+            <SegmentedTabs items={items} block />
+          </div>
         </Modal>
-      </>
+      </Space>
     );
   }
   return (
