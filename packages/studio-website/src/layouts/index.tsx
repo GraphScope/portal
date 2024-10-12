a55f72da89d16ce322cb9d2159c1604ec5d00f85
pomelo-nwu <czynwu@outlook.com>
2024-10-12 16:02:59 +0800

    feat: update nav

--- packages/studio-website/src/layouts/index.tsx
+++ packages/studio-website/src/layouts/index.tsx
@@ -3,15 +3,11 @@ import { Outlet } from 'react-router-dom';
 import './index.less';
 import Sidebar from './sidebar';
 import Container from './container';
-
 import Footer from './footer';
-import locales from '../locales';
 import { useContext } from './useContext';
 import { TOOLS_MENU } from './const';
 import SegmentedSection from './segmented-section';
 
-import type { MenuProps } from 'antd';
-import { StudioProvier } from '@graphscope/studio-components';
 const Content = (props: any) => {
   const { children, navStyle } = props;
   const { store } = useContext();
@@ -35,16 +31,14 @@ export default function Layout() {
   const { navStyle } = store;
 
   return (
-    <StudioProvier locales={locales}>
-      <Container
-        sidebar={<Sidebar />}
-        content={
-          <Content navStyle={navStyle}>
-            <Outlet />
-          </Content>
-        }
-        footer={<Footer />}
-      />
-    </StudioProvier>
+    <Container
+      sidebar={<Sidebar />}
+      content={
+        <Content navStyle={navStyle}>
+          <Outlet />
+        </Content>
+      }
+      footer={<Footer />}
+    />
   );
 }
