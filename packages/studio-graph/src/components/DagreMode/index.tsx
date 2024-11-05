a0e66e0d9608edf130517982df340cfe7c271599
pomelo-nwu <czynwu@outlook.com>
2024-11-05 17:23:24 +0800

    feat: add dagre mode

---
+++ packages/studio-graph/src/components/DagreMode/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Button, Tooltip } from 'antd';
+import { useContext } from '../../hooks/useContext';
+import { BranchesOutlined } from '@ant-design/icons';
+
+interface IDagreModeProps {}
+
+const DagreMode: React.FunctionComponent<IDagreModeProps> = props => {
+  const { store } = useContext();
+  const { graph } = store;
+  const [dagreMode, setToogle] = React.useState(false);
+  const handleClick = () => {
+    if (graph) {
+      if (!dagreMode) {
+        graph.dagMode('lr');
+      } else {
+        graph.dagMode(null);
+      }
+    }
+    setToogle(!dagreMode);
+  };
+  const title = dagreMode ? 'Force Mode' : 'Dagre Mode';
+
+  return (
+    <Tooltip title={title} placement="left">
+      <Button onClick={handleClick} icon={<BranchesOutlined />} type="text" />
+    </Tooltip>
+  );
+};
+
+export default DagreMode;
