f2185ee37149ec2dd6ce52f8374836f04c287e1a
louyk18 <14280048+louyk18@user.noreply.gitee.com>
2024-11-22 20:00:03 +0800

    do not delete collection

--- python/graphy/memory/llm_memory.py
+++ python/graphy/memory/llm_memory.py
@@ -406,7 +406,7 @@ class RetrievedMemory:
                     if collection_id in cache:
                         segment_manager.callback_cache_evict(cache[collection_id])
 
-            self.persistent_client.delete_collection(name=self.collection_name)
+            # self.persistent_client.delete_collection(name=self.collection_name)
             gc.collect()
 
         print(f"========== AFTER UNLOAD {get_process_info()} =========")
