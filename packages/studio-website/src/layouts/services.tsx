3624d904f03b65792a521b18d9a93fc5c5bc54c9
pomelo-nwu <czynwu@outlook.com>
2024-11-22 17:36:19 +0800

    feat: add Feature case

--- packages/studio-website/src/layouts/services.tsx
+++ packages/studio-website/src/layouts/services.tsx
@@ -8,6 +8,51 @@ import {
 
 import { notification } from '../pages/utils';
 
+export type SupportFeature =
+  /** 是否支持多图 */
+  | 'MULTIPLE_GRAPHS'
+  /** 是否支持批量创建图模型 */
+  | 'BATCH_CREATE_SCHEMA'
+  /** schema是否支持更新修改 */
+  | 'SCHEMA_UPDATE'
+  /** 是否支持CSV导图 */
+  | 'LOAD_CSV_DATA'
+  /** 是否支持查询多版本 */
+  | 'QUERY_MULTIPLE_VERSIONS';
+
+type Features = {
+  [K in SupportFeature]: boolean;
+};
+
+export const getSupportFeature = (): Features => {
+  if (window.GS_ENGINE_TYPE === 'groot') {
+    return {
+      BATCH_CREATE_SCHEMA: false,
+      LOAD_CSV_DATA: false,
+      MULTIPLE_GRAPHS: false,
+      QUERY_MULTIPLE_VERSIONS: false,
+      SCHEMA_UPDATE: true,
+    };
+  }
+  if (window.GS_ENGINE_TYPE === 'gart') {
+    return {
+      BATCH_CREATE_SCHEMA: true,
+      LOAD_CSV_DATA: false,
+      MULTIPLE_GRAPHS: false,
+      QUERY_MULTIPLE_VERSIONS: true,
+      SCHEMA_UPDATE: false,
+    };
+  }
+  //interactive
+  return {
+    BATCH_CREATE_SCHEMA: true,
+    LOAD_CSV_DATA: true,
+    MULTIPLE_GRAPHS: true,
+    QUERY_MULTIPLE_VERSIONS: false,
+    SCHEMA_UPDATE: false,
+  };
+};
+
 export const listGraphs = async () => {
   const status = await ServiceApiFactory(undefined, location.origin)
     .listServiceStatus()
--- packages/studio-website/src/pages/instance/index.tsx
+++ packages/studio-website/src/pages/instance/index.tsx
@@ -5,6 +5,7 @@ import Section from '../../components/section';
 import { PlusOutlined } from '@ant-design/icons';
 import { FormattedMessage } from 'react-intl';
 import CreateInstance from './create';
+import FeatureCase from '../../components/feature-case';
 const Instance: React.FunctionComponent = () => {
   return (
     <Section
@@ -16,7 +17,9 @@ const Instance: React.FunctionComponent = () => {
       style={{ padding: '0px 20px' }}
     >
       <Flex align="end" justify="flex-start" style={{ padding: '16px 0px' }}>
-        <CreateInstance />
+        <FeatureCase match="MULTIPLE_GRAPHS">
+          <CreateInstance />
+        </FeatureCase>
       </Flex>
       <InstanceLists />
     </Section>
