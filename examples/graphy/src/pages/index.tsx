c2e8e4aa254e797444d5e793e996a7fea2dc2097
pomelo-nwu <czynwu@outlook.com>
2024-11-25 15:46:29 +0800

    feat: add graphy into experimental tools

--- examples/graphy/src/pages/index.tsx
+++ examples/graphy/src/pages/index.tsx
@@ -1,11 +1,5 @@
-import React, { Suspense, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate, Outlet, HashRouter } from 'react-router-dom';
-import { Layout } from '@graphscope/studio-components';
-import { SIDE_MENU } from './const';
-import { ConfigProvider } from 'antd';
-import locales from '../locales';
-import { IntlProvider } from 'react-intl';
-import PaperReading from '../pages/explore/paper-reading';
+import React, { Suspense } from 'react';
+import { Route, Navigate } from 'react-router-dom';
 
 /**  注册 服务 */
 import { registerServices } from '../pages/explore/paper-reading/components/registerServices';
@@ -56,69 +50,24 @@ const routes = [
     path: '/dataset/cluster',
     component: React.lazy(() => import('./dataset/cluster')),
   },
-  { path: '/explore', component: React.lazy(() => import('./explore')) },
 ];
 
-const Apps = () => {
-  const [isReady, setIsReady] = React.useState(false);
-  useEffect(() => {
-    reload().then(res => {
-      setIsReady(true);
-    });
-  }, []);
+const ROUTES = routes.map(({ path, redirect, component: Component }, index) => {
+  if (redirect) {
+    return <Route key={index} path={path} element={<Navigate to={redirect} replace />} />;
+  }
   return (
-    <>
-      <Outlet />
-      {isReady && <PaperReading />}
-    </>
+    <Route
+      key={index}
+      path={path}
+      element={
+        <Suspense fallback={<></>}>
+          {/** @ts-ignore */}
+          <Component />
+        </Suspense>
+      }
+    />
   );
-};
-const Pages: React.FunctionComponent<IPagesProps> = props => {
-  const locale = 'en-US';
-  const messages = locales[locale];
-  const routeComponents = routes.map(({ path, redirect, component: Component }, index) => {
-    if (redirect) {
-      return <Route key={index} path={path} element={<Navigate to={redirect} replace />} />;
-    }
-    return (
-      <Route
-        key={index}
-        path={path}
-        element={
-          <Suspense fallback={<></>}>
-            {/** @ts-ignore */}
-            <Component />
-          </Suspense>
-        }
-      />
-    );
-  });
+});
 
-  return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Menu: {
-            itemSelectedBg: '#ececec',
-            itemSelectedColor: '#191919',
-            collapsedWidth: 50,
-            collapsedIconSize: 14,
-          },
-        },
-      }}
-    >
-      <IntlProvider messages={messages} locale={locale}>
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<Layout sideMenu={[SIDE_MENU]} />}>
-              {routeComponents}
-            </Route>
-            <Route path={'/paper-reading'} element={<Apps />} />
-          </Routes>
-        </HashRouter>
-      </IntlProvider>
-    </ConfigProvider>
-  );
-};
-
-export default Pages;
+export default ROUTES;
