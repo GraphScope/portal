bf3714896e7ffcaefc006d40318b8dc2e848b002
pomelo-nwu <czynwu@outlook.com>
2024-12-18 16:44:36 +0800

    feat: support gremlin neighbor query

--- packages/studio-query/src/components/query-graph/gremlin-services/queryNeighbor.tsx
+++ packages/studio-query/src/components/query-graph/gremlin-services/queryNeighbor.tsx
@@ -1,66 +1,20 @@
 import type { INeighborQueryData, INeighborQueryItems } from '@graphscope/studio-graph';
 import { queryStatement } from '../queryStatement';
 export const queryNeighborData: INeighborQueryData['query'] = async params => {
-  const { key, selectIds } = params;
-  const script = `
-    MATCH ${key}
-    WHERE  elementId(a) IN [${selectIds}] 
-    RETURN a,b,c
-    `;
-  // const data = await queryStatement(script);
-  return {
-    nodes: [],
-    edges: [],
-  };
+  const { selectIds } = params;
+  const ids = selectIds.join(',');
+  const script = `g.V().hasId(${ids}).both().dedup()`;
+  const data = await queryStatement(script);
+  return data;
 };
 
 export const queryNeighborItems: INeighborQueryItems['query'] = async params => {
-  const { schema } = params;
-  const itemMap = {};
-  schema.nodes.forEach(node => {
-    itemMap[node.label] = getOptionsBySchema(schema, node.label);
-  });
-  return itemMap;
+  return {
+    all: [
+      {
+        key: `(a)-[b]-(c)`,
+        label: `One-Hop Neighbors`,
+      },
+    ],
+  };
 };
-
-function getOptionsBySchema(schema, nodeLabel) {
-  const options: { key: string; label: string }[] = [];
-  schema.edges.forEach(item => {
-    let direction = 'out';
-    const { source, target, label } = item;
-    const include = nodeLabel === source || nodeLabel === target;
-    if (include && source === target) {
-      direction = 'both';
-      options.push({
-        key: `(a:${source})-[b:${label}]-(c:${target})`,
-        label: `[${label}]-(${target})`,
-      });
-      return;
-    }
-    if (source === nodeLabel) {
-      direction = 'out';
-      options.push({
-        key: `(a:${source})-[b:${label}]->(c:${target})`,
-        label: `[${label}]->(${target})`,
-      });
-    }
-    if (target === nodeLabel) {
-      direction = 'in';
-      options.push({
-        key: `(a:${source})<-[b:${label}]-(c:${target})`,
-        label: `[${label}]<-(${target})`,
-      });
-    }
-  });
-
-  const extraItems =
-    options.length > 1
-      ? [
-          {
-            key: `(a)-[b]-(c)`,
-            label: `All Neighbors`,
-          },
-        ]
-      : [];
-  return [...extraItems, ...options];
-}
