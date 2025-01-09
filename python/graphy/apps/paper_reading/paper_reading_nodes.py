73cb0e0875f39cd72583981a3e3e841eaa07f8d7
longbin.lailb <longbin.lailb@alibaba-inc.com>
2025-01-09 23:09:25 +0800

    fix bug

--- python/graphy/apps/paper_reading/paper_reading_nodes.py
+++ python/graphy/apps/paper_reading/paper_reading_nodes.py
@@ -647,7 +647,8 @@ class PaperInspector(BaseNode):
 
                 all_nodes = set(self.graph.get_node_names())
                 persist_states = set(self.persist_store.get_total_states(data_id))
-                if persist_states == all_nodes:
+
+                if all_nodes.issubset(persist_states):
                     # This means that the data has already processed
                     logger.info(f"Input with ID '{data_id}' already processed.")
                     self.progress["total"].add(
