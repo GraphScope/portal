9ce8693ca2bb78c366390434e6952565672a772f
louyk18 <14280048+louyk18@user.noreply.gitee.com>
2025-01-13 13:39:46 +0800

    fix empty id

--- python/graphy/utils/paper_struct.py
+++ python/graphy/utils/paper_struct.py
@@ -118,9 +118,9 @@ class Paper:
                     elif key in Paper.str_header():
                         parsed_meta[key] = ""
 
-        parsed_meta["id"] = parsed_meta.get(
-            "id", str(hash(parsed_meta.get("title", "").lower()))
-        )
+        paper_id = parsed_meta.get("id", "")
+        if paper_id == "":
+            parsed_meta["id"] = str(hash(parsed_meta.get("title", "").lower()))
 
         if not parsed_meta.get("published", ""):
             try:
