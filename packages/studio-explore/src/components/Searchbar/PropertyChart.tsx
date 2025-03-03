47a3ba7c43035a1c43f0775ed193b99ea7e8bd31
pomelo-nwu <czynwu@outlook.com>
2025-03-03 14:13:29 +0800

    feat: update code

--- packages/studio-explore/src/components/Searchbar/PropertyChart.tsx
+++ packages/studio-explore/src/components/Searchbar/PropertyChart.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Select, Space, Flex, Card, Button, Typography, Skeleton } from 'antd';
+import { Select, Space, Flex, Card, Button, Typography, Skeleton, theme } from 'antd';
 import { useContext, IQueryStatement } from '@graphscope/studio-graph';
 import { BarChartOutlined, DeleteOutlined } from '@ant-design/icons';
 import Chart from '../ChartView/index';
+import Setting from './PropertyChartSetting';
 
 import { Illustration } from '@graphscope/studio-components';
 export interface IQueryPropertyStatics {
@@ -17,6 +18,7 @@ interface ITableViewProps {
 const ChartView: React.FunctionComponent<ITableViewProps> = props => {
   const { store, updateStore } = useContext();
   const { getService, schema } = store;
+  const { token } = theme.useToken();
 
   const [state, setState] = useState<{
     data: { [key: string]: any };
@@ -82,29 +84,39 @@ const ChartView: React.FunctionComponent<ITableViewProps> = props => {
     if (!property) {
       return;
     }
+
+    const property_keys = e.map(item => {
+      return item[property];
+    });
+
     const queryCypher = getService<IQueryStatement>('queryStatement');
-    console.log(
-      'e.data.data[property]',
-      e.data.data[property],
-      `
-      MATCH(a:${label}) 
-      WHERE a.${property}="${e.data.data[property]}"
-      return a
-      `,
-    );
-    const data = await queryCypher(
-      `
-        MATCH(a:${label}) 
-        WHERE a.${property}="${e.data.data[property]}"
-        return a
-        `,
-    );
+    const script = `MATCH(a:${label}) WHERE a.${property} in ${JSON.stringify(property_keys)} return a`;
+    console.log('%c[QueryStatement:Chart]', 'color:red', script);
+    const data = await queryCypher(script);
 
     updateStore(draft => {
       draft.data = data;
       draft.source = data;
     });
   };
+  console.log('data', data, isLoading);
+  if (isLoading) {
+    return <Skeleton active style={{ padding: '24px' }} />;
+  }
+  if (data && data.length === 0) {
+    return (
+      <Flex vertical>
+        <Typography.Text type="secondary">
+          This property lacks statistical significance,please search directly
+          <Illustration.FunArrow style={{ height: '20px', width: '26px', color: token.colorPrimary }} />
+        </Typography.Text>
+        <Typography.Text type="secondary">
+          If you need a statistical chart, please set up the statistical fields <Setting />
+        </Typography.Text>
+        <Illustration.Charts style={{ height: '160px', width: '160px' }} />
+      </Flex>
+    );
+  }
 
   return (
     <Flex vertical style={{ height: '400px' }}>
@@ -112,18 +124,7 @@ const ChartView: React.FunctionComponent<ITableViewProps> = props => {
         You can click on the `{property}` charts below to start a query, or use the search bar above for detailed
         searches.
       </Typography.Text>
-      {isLoading && <Skeleton active style={{ padding: '24px' }} />}
-      {!isLoading &&
-        (property ? (
-          <Chart data={data} xField={property} yField="counts" onClick={handleChartClick} />
-        ) : (
-          <Flex vertical justify="center" align="center">
-            <Illustration.Charts style={{ height: '160px', width: '160px' }} />
-            <Typography.Text type="secondary" style={{ textAlign: 'center' }}>
-              Select the properties you're interested in
-            </Typography.Text>
-          </Flex>
-        ))}
+      <Chart data={data} xField={property} yField="counts" onClick={handleChartClick} />
     </Flex>
   );
 };
--- packages/studio-explore/src/components/Setting/index.tsx
+++ packages/studio-explore/src/components/Setting/index.tsx
@@ -1,12 +1,52 @@
-import * as React from 'react';
-import { Typography, Flex } from 'antd';
-interface ISettingProps {}
+import React, { useEffect, useState } from 'react';
+import { useContext } from '@graphscope/studio-graph';
+import { SplitSection, Utils } from '@graphscope/studio-components';
+import { theme, Flex, Input, Modal, Button, Typography, Divider, Row, Col, Card } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
 
-const Setting: React.FunctionComponent<ISettingProps> = props => {
+import { ConnectEndpoint } from '@graphscope/studio-query';
+
+interface IInitProps {}
+
+const Setting: React.FunctionComponent<IInitProps> = props => {
+  const { id, store } = useContext();
+  const { token } = theme.useToken();
+  const [visible, setVisible] = React.useState(false);
+
+  const handleToggle = () => {
+    setVisible(!visible);
+  };
+  const handleClose = () => {
+    setVisible(false);
+  };
+  const handleConnect = params => {
+    handleClose();
+  };
+
   return (
-    <Flex>
-      <Typography.Text>Settings</Typography.Text>
-    </Flex>
+    <>
+      <Button onClick={handleToggle} icon={<SettingOutlined />} type="text"></Button>
+      <Modal open={visible} onClose={handleClose} onCancel={handleClose} width={'80%'} footer={null}>
+        <Flex vertical gap={12}>
+          <Typography.Title level={3}>Settings</Typography.Title>
+
+          <Row gutter={[12, 12]}>
+            <Col span={12}>
+              <Card>
+                <Typography.Title level={5}>Searchbar</Typography.Title>
+                <Typography.Text type="secondary">dddd</Typography.Text>
+              </Card>
+            </Col>
+            <Col span={12}>
+              {/* <Card>
+                <Typography.Title level={5}>Searchbar</Typography.Title>
+                <Typography.Text type="secondary">dddd</Typography.Text>
+              </Card> */}
+            </Col>
+          </Row>
+        </Flex>
+      </Modal>
+    </>
   );
 };
 
