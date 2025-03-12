aac57d4bb8bef3d1fc69fb461c8cfeca44ea855f
xwj02155382 <xwj02155382@alibaba-inc.com>
2025-03-12 15:00:53 +0800

    fix: label modify

--- packages/studio-website/src/pages/instance/lists/server-not-available.tsx
+++ packages/studio-website/src/pages/instance/lists/server-not-available.tsx
@@ -6,19 +6,12 @@ const { Text, Title } = Typography;
 
 const ServerNotAvailable: React.FC = () => {
   const { token } = theme.useToken();
-  const commandText = [
-    '# Pull the GraphScope Interactive Docker image',
-    'docker pull registry.cn-hongkong.aliyuncs.com/graphscope/interactive',
-    '',
-    '# Start the GraphScope Interactive service',
-    `docker run -d --name gs -p 8080:8080 -p 7777:7777 -p 10000:10000 -p 7687:7687 registry.cn-hongkong.aliyuncs.com/graphscope/interactive --enable-coordinator --port-mapping "8080:8080,7777:7777,10000:10000,7687:7687"`,
-  ].join('\n');
   return (
     <Row gutter={[12, 12]}>
       <Col span={24}>
         <Card>
           <Flex>
-            <Result status="404" style={{ transform: 'scale(0.9)' }} />
+            <Result status="404" />
             <Flex vertical justify="flex-start" style={{ flex: 1 }}>
               <Title level={3}>
                 <FormattedMessage id="No available Coordinator service" />
@@ -33,16 +26,19 @@ const ServerNotAvailable: React.FC = () => {
                   height: '160px',
                   borderRadius: token.borderRadius,
                   marginTop: 20,
-                  padding: 20,
+                  padding: '20px 20px 0',
                 }}
               >
                 <pre
                   style={{
                     color: token.colorTextLightSolid,
-                    whiteSpace: 'pre-wrap',
+                    whiteSpace: 'pre-wrap', 
                   }}
-                >
-                  {commandText}
+                >{`# Pull the GraphScope Interactive Docker image
+docker pull registry.cn-hongkong.aliyuncs.com/graphscope/interactive
+
+# Start the GraphScope Interactive service
+docker run -d --name gs -p 8080:8080 -p 7777:7777 -p 10000:10000 -p 7687:7687 registry.cn-hongkong.aliyuncs.com/graphscope/interactive --enable-coordinator --port-mapping "8080:8080,7777:7777,10000:10000,7687:7687"`}
                 </pre>
               </Flex>
             </Flex>
