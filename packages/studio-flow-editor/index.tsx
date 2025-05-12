0a3d78f90b343ff0ec3bfbb8f0a727813fbfeeda
xwj02155382 <xwj02155382@alibaba-inc.com>
2025-05-12 11:11:14 +0800

    feat: add flow-editor test demo

--- packages/studio-flow-editor/index.tsx
+++ packages/studio-flow-editor/index.tsx
@@ -1,31 +1,35 @@
 import * as React from 'react';
-import { GraphProvider, GraphCanvas, useGraphStore } from './src/index';
+import { GraphProvider, GraphCanvas, useGraphStore, useAddNode } from './src/index';
 import { createRoot } from 'react-dom/client';
-import { Divider ,Button} from 'antd';
-import StoreProvider from '@graphscope/use-zustand';
+import { Button } from 'antd';
 
 interface IAppProps {}
-const Edit = ()=>{
-  const {store,} = useGraphStore();
+const Edit = () => {
+  const { store } = useGraphStore();
+  const { handleAddVertex } = useAddNode();
   const { nodes } = store;
-  const printData = ()=>{
-  console.log('nodes::: ', nodes);
-    console.log(JSON.stringify(store))
-  }
-  return  (
-    <GraphCanvas>
-      <Button style={{position:"absolute",zIndex:4}} onClick={()=>printData()}>
-          test
+  const printData = () => {
+    console.log('nodes::: ', nodes);
+  };
+  return (
+    <>
+      <Button style={{ position: 'absolute', zIndex: 4, left: 20, top: 20 }} onClick={() => printData()}>
+        test
       </Button>
-    </GraphCanvas>
-  )
-}
+      <Button style={{ position: 'absolute', zIndex: 4, left: 80, top: 20 }} onClick={() => handleAddVertex()}>
+        add
+      </Button>
+    </>
+  );
+};
 const DrawGraph: React.FunctionComponent<IAppProps> = props => {
   return (
     <>
       <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
         <GraphProvider>
-           <Edit/>
+          <GraphCanvas>
+            <Edit />
+          </GraphCanvas>
         </GraphProvider>
       </div>
     </>
